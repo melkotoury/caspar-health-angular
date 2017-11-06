a4c8d6253f11461cb6f270e10d1152a7d18da6b6
melkotoury <melkotoury@gmail.com>
2017-11-06 19:02:37 +0200

    feat: add clinic listing visible to all users

--- src/app/clinics/clinic.component.ts
+++ src/app/clinics/clinic.component.ts
@@ -1,3 +1,5 @@
+import { ClinicsService } from './../services/clinics.service';
+import {Clinics} from '../model/clinics';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clinic.component.css']
 })
 export class ClinicComponent implements OnInit {
+allClinics: Clinics[];
+statusCode: number;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+// Create constructor to get service instance
+constructor(private clinicsService: ClinicsService) {}
+// Create ngOnInit() and and load clinics
+ngOnInit(): void {
+  this.getAllClinics();
+}
+// Fetch all clinics
+getAllClinics() {
+  this
+    .clinicsService
+    .getAllClinics()
+    .subscribe(data => this.allClinics = data, errorCode => this.statusCode = errorCode);
+}
 }
